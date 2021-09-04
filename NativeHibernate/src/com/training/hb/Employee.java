f78e55df27b6918808a1d6a74127de31bebc0a83
Arokia-Bhavya <arokia.bhavya@gmail.com>
2021-09-04 11:28:57 +0530

    hibernate

---
+++ NativeHibernate/src/com/training/hb/Employee.java
@@ -0,0 +1,23 @@
+package com.training.hb;
+
+
+public class Employee {  
+private int Id;  
+public int getId() {
+	return Id;
+}
+public void setId(int id) {
+	Id = id;
+}
+private String name;  
+  
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+
+
+  
+}  
